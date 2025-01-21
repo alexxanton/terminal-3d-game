df5007a4d05ae6c254637badcf2e6c69a55a942c
meanoldgit <alexantonaranda@gmail.com>
2025-01-21 22:52:18 +0100

    added wall with shades for depth perception

--- src/main/java/com/terminal/game3d/GameArea.java
+++ src/main/java/com/terminal/game3d/GameArea.java
@@ -4,24 +4,27 @@ public class GameArea {
     private Screen screen = new Screen();
     private char[][][] gameArea = new char[screen.getDepth()][screen.getHeight()][screen.getWidth()];
     private Player player = new Player(screen.getWidth(), screen.getHeight(), screen.getDepth());
-    private final char PLAYER_BLOCK = '▄';
-    private final char BIG_BLOCK = '█';
+    private final char PLAYER_BLOCK = '█';
+    private final char LIGHT_SHADE = '▒';
+    private final char MEDIUM_SHADE = '▒';
+    private final char DARK_SHADE = '▓';
     private final char EMPTY_SPACE = ' ';
     private Thread gameLoop = new Thread(() -> gameLoop());
+    private final char[] WALL_SHADES = {DARK_SHADE, MEDIUM_SHADE, LIGHT_SHADE, LIGHT_SHADE, EMPTY_SPACE};
     
 
     public GameArea() {
         for (int z = 0; z < gameArea.length; z++) {
             for (int y = 0; y < gameArea[z].length; y++) {
                 for (int x = 0; x < gameArea[z][y].length; x++) {
-                    gameArea[z][y][x] = ' ';
+                    gameArea[z][y][x] = WALL_SHADES[z];
                 }
             }
         }
     }
      
     private void updatePlayer() {
-        gameArea[player.getZ()][player.getY()][player.getX()] = EMPTY_SPACE;
+        gameArea[player.getZ()][player.getY()][player.getX()] = WALL_SHADES[player.getZ()];
         player.move();
         gameArea[player.getZ()][player.getY()][player.getX()] = PLAYER_BLOCK;
     }
