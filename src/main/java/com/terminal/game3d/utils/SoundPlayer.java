ba9a3dd9104f4e020e29036706b422330e71d5bd
meanoldgit <alexantonaranda@gmail.com>
2025-01-23 10:35:36 +0100

    sound player improved

--- src/main/java/com/terminal/game3d/utils/SoundPlayer.java
+++ src/main/java/com/terminal/game3d/utils/SoundPlayer.java
@@ -2,18 +2,35 @@ package com.terminal.game3d.utils;
 
 import javax.sound.sampled.*;
 import java.io.File;
-import java.io.IOException;
 
 public class SoundPlayer {
     public static void playSound(String soundFilePath) {
-        try {
-            File soundFile = new File(soundFilePath);
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioInputStream);
-            clip.start();
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-            e.printStackTrace();
-        }
+        new Thread(() -> {
+            try {
+                File soundFile = new File(soundFilePath);
+                if (!soundFile.exists()) {
+                    System.err.println("Sound file not found: " + soundFilePath);
+                    return;
+                }
+
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+                AudioFormat format = audioInputStream.getFormat();
+                DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+                SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+                line.open(format);
+                line.start();
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = audioInputStream.read(buffer)) != -1) {
+                    line.write(buffer, 0, bytesRead);
+                }
+
+                line.drain();
+                line.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
